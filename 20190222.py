55146c7f0e599a87248899067b526d3d12cbe659
Robert-Jan <lub@countcooper.com>
2019-02-22 10:23:41 +0800

    first commit

---
+++ 20190222.py
@@ -0,0 +1,13 @@
+names = ["Wim", "Alexander", "Robbert-Jan", "Wouter", "Jurre", "Victor", "Peter", "Jeroen", "Sander", "Douwe", "Melvin",
+         "Stefan", "Willem"]
+
+for name in names:
+    print(name)
+
+for name in names:
+    print(f'Hello, {name}! How are you today?')
+
+cars = ["Ferrari", "Lamborghini", "BMW", "Mercedes", "Audi"]
+
+for car in cars:
+    print(f"I would like to own a {car}.")
